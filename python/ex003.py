b263e775b0b167b5ae1d043eec412756fe3382c3
Caiojaqueri <caiojaqueri.contato@gmail.com>
2025-10-11 22:00:34 -0300

    realizei o exercício 3

---
+++ python/ex003.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+soma = n1 + n2
+print('A soma entre', n1, 'e', n2, 'é igual a', soma)
+# o mais da variável soma está concatenando as strings e não somando os números, emtão é necessario converter os números digitados em tipos inteiros
