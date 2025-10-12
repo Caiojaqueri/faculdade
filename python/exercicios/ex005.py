6682aeecc45f6dde454d70070fed3cd34dff5123
Caiojaqueri <caiojaqueri.contato@gmail.com>
2025-10-12 12:05:43 -0300

    exercício 005

--- python/exercicios/ex005.py
+++ python/exercicios/ex005.py
@@ -1,2 +1,6 @@
-n1 = float(input('Digite um valor: '))
-print(type(n1))
+n1 = input('Digite um algo: ')
+#print(n1.isupper()) - se n1 está em maiúsculas
+#print(n1.islower()) - se n1 está em minúsculas
+#print(n1.isalnum()) - se n1 é alfanumérico
+#print(n1.isalpha()) - se n1 é alfabético
+#print(n1.isnumeric()) - se n1 é numérico
