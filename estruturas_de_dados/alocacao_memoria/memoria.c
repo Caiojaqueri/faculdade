35240878ee4c5e72fba28bee1ac75773ea37bb91
Caiojaqueri <caiojaqueri.contato@gmail.com>
2025-10-29 10:22:19 -0300

    exercício sobre alocação de memória

---
+++ estruturas_de_dados/alocacao_memoria/memoria.c
@@ -0,0 +1,7 @@
+
+//int *a, b;
+//b = 10;
+//a = (int*)malloc(sizeof(int)); - pedimos um espaço
+//*a = 20;
+//free(a); - invertemos o free para ca para liberar o espaço antes de trocar o a para o espaço b
+//a = &b;
