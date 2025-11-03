fd517a2bb74d27d02632479f26f7aae1b6b30121
Caiojaqueri <caiojaqueri.contato@gmail.com>
2025-11-03 13:50:16 -0300

    finalizei o desafio nivel mestre

--- estruturas_de_dados_emC/desafios/desafio_mestre/war.c
+++ estruturas_de_dados_emC/desafios/desafio_mestre/war.c
@@ -62,3 +62,82 @@ void liberarMemoria(territorio *mapa, char *missao) {
 }
 
 //FUNÇÃO PRINCIPAL
+int main() {
+    srand(time(NULL));
+
+    //missões
+    char *missoes[] = {
+        "Conquistar 3 territórios seguidos.",
+        "Eliminar todas as tropas da cor Vermelha.",
+        "Destruir o exercito Verde.",
+        "Dominar Ameria e Europa.",
+        "Conquistar todos os terriorios Azuis."
+    };
+    int totalMissoes = 5;
+
+    //territorios
+    int qtdTerritorios = 5;
+    territorio *mapa = (territorio *) calloc(qtdTerritorios, sizeof(territorio));
+    strcpy(mapa[0].nome, "America");
+    strcpy(mapa[0].cor, "Verde");
+    mapa[0].tropas = 3;
+
+    strcpy(mapa[1].nome, "Europa");
+    strcpy(mapa[1].cor, "Azul");
+    mapa[1].tropas = 3;
+
+    strcpy(mapa[2].nome, "Asia");
+    strcpy(mapa[2].cor, "Vermelho");
+    mapa[2].tropas = 3;
+
+    strcpy(mapa[3].nome, "Africa");
+    strcpy(mapa[3].cor, "Amarelo");
+    mapa[3].tropas = 3;
+
+    strcpy(mapa[4].nome, "Oceania");
+    strcpy(mapa[4].cor, "Branco");
+    mapa[4].tropas = 3;
+
+    //sorteio da missao 
+    char *missaoJogador = (char *) malloc(100 * sizeof(char));
+    atribuirMissao(missaoJogador, missoes, totalMissoes);
+
+    printf("\n---SUA MISSAO (Exercito Azul)---\n%s\n", missaoJogador);
+
+    int opcao;
+    do {
+        printf("\n--- MENU DE ACOES ---\n");
+        printf("1 - Atacar\n");
+        printf("2 - Verificar Missao\n");
+        printf("0 - Sair\n");
+        printf("Escolha sua acao: ");
+        scanf("%d", &opcao);
+
+        if(opcao == 1) {
+            int ataque, defesa;
+            exibirMapa(mapa, qtdTerritorios);
+            printf("Escolha territorio atacante (numero): ");
+            scanf("%d", ataque);
+            printf("Escolha territorio defensor (numero): ");
+            scanf("%d", defesa);
+
+            if(ataque >= 1 && ataque <= qtdTerritorios && defesa>= 1 && defesa <= qtdTerritorios) {
+                atacar(&mapa[ataque - 1], &mapa[defesa - 1]);
+            }
+            else if (opcao == 2) {
+                if (verificarMissao(missaoJogador, mapa, qtdTerritorios))
+                    printf("\n*** MISSAO CUMPRIDA! VOCE VENCEU! ***\n");
+                    break;
+            } else {
+                printf("\n Missai ainda nao cumprida. \n");
+            }
+
+        }
+
+        printf("\nPressione Enter para continuar...");
+        getchar(); getchar();
+    } while (opcao != 0);
+    liberarMemoria(mapa, missaoJogador);
+    printf("\nJogo encerrado.\n");
+    return 0;
+}
