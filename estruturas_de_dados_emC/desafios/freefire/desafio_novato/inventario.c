0e03462e46a079d81d1d8683370313adab77f665
Caiojaqueri <caiojaqueri.contato@gmail.com>
2025-11-06 12:13:54 -0300

    iniciei o desafio nível novato freefire

---
+++ estruturas_de_dados_emC/desafios/freefire/desafio_novato/inventario.c
@@ -0,0 +1,14 @@
+// criação de um sistema de inventario para melhor estratégia para o jogo freefire
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+
+struct item {
+    char nome[30];
+    char tipo[20];
+    int quantidade;
+};
+
