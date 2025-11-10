adbe538424f65c18c41784fd0d09af580da5eb85
Caiojaqueri <caiojaqueri.contato@gmail.com>
2025-11-10 18:58:27 -0300

    iniciei criando as funções do programa de inventário

--- estruturas_de_dados_emC/desafios/freefire/desafio_novato/inventario.c
+++ estruturas_de_dados_emC/desafios/freefire/desafio_novato/inventario.c
@@ -4,11 +4,37 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define MAX_ITENS 10
 
-
-struct item {
+struct Item {
     char nome[30];
     char tipo[20];
     int quantidade;
 };
 
+//Vetor global de itens e variávem para controlar o número total de itens
+struct Item mochila[MAX_ITENS];
+int totalItens = 0;
+
+//função para cadastrar um novo item
+void inserirItem() {
+    if (totalItens >= MAX_ITENS) {
+        printf("\nA mochila está cheia!\n");
+        return;
+    }
+
+    struct Item novoItem;
+    printf("\n--- Cadastro de Item ---\n");
+    printf("Nome: ");
+    scanf("%s", novoItem.nome);
+
+    printf("Tipo: ");
+    scanf("%s", novoItem.tipo);
+
+    printf("Quantidade: ");
+    scanf("%d", &novoItem.quantidade);
+
+    mochila[totalItens] = novoItem;
+    totalItens++;
+    
+}
